7e457d988f8269c5f1b30d80e1ad88868d38d761
Christof <christof@fitrockr.com>
2022-08-10 08:58:09 +0200

    Import

--- server/src/main/java/de/landscapr/server/repository/RepoService.java
+++ server/src/main/java/de/landscapr/server/repository/RepoService.java
@@ -280,7 +280,7 @@ public class RepoService {
                 generator.writeString(tag);
             }
             generator.writeEndArray();
-            generator.writeNumberField("implementationType", item.getImplementationType());
+            generator.writeNumberField("implementationType", item.getImplementationType() == null ? 0 : item.getImplementationType());
             generator.writeNumberField("status", item.getStatus() == null ? 0 : item.getStatus());
             generator.writeEndObject();
         }
