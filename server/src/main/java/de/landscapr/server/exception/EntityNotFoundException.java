4cd3e4174a51d48988cf9109cccc3d4071f4c4a9
christof-fitrockr <154627890+christof-fitrockr@users.noreply.github.com>
2025-11-08 13:01:03 +0100

    Restructured

--- server/src/main/java/de/landscapr/server/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.landscapr.server.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String msg) {
-        super(msg);
-    }
-}
